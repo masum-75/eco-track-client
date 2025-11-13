34dc4f2f557a23ffc40cc664ad06926e738c67dd
masum-75 <tanbirmasum014@gmail.com>
2025-11-13 23:11:43 +0600

    add not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function NotFound() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-3xl font-bold">404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
